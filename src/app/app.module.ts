7ad7cb426fa2b8d1781bf48428d159a4a8c77bd0
Ercetei <tournier.r@gmail.com>
2018-08-30 21:18:58 +0200

    #67 - ajout gestion utilisateur - connexion / inscription + service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { UserBlockComponent } from './user-block/user-block.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { UserService } from './shared/user.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { appRoutes } from './routes';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthInterceptor } from './auth/auth.interceptor';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserBlockComponent
+    SignUpComponent,
+    UserComponent,
+    SignInComponent,
+    HomeComponent
   ],
   imports: [
-    NgbModule,
     BrowserModule,
-    AngularFontAwesomeModule
+    FormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [UserService,AuthGuard,
+    ,
+    {
+      provide : HTTP_INTERCEPTORS,
+      useClass : AuthInterceptor,
+      multi : true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
